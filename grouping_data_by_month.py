052e2e6a0259e99c7e32ecce4ea41eccb7d78220
Md. Samshad Rahman <samshad.ru@gmail.com>
2019-11-26 01:04:12 +0600

    Grouped by month

--- grouping_data_by_month.py
+++ grouping_data_by_month.py
@@ -2,53 +2,52 @@ import pandas as pd
 import os
 
 
+def write_file(username, year, data, fname):
+    dataframe = pd.DataFrame(data, columns=['tweet_id', 'date', 'title', 'category', 'rating', 'urls'])
+    dataframe['date'] = pd.to_datetime(dataframe['date'])
+    dataframe.sort_values(by=['date'], inplace=True, ascending=False)
+    dataframe.to_csv('Data/Grouped_Datas/' + username + '/' + year + '/' + fname + '.csv', index=None, header=True)
+
+
 path = 'Data/Foursq_Data/'
 files = os.listdir(path)
 files.sort()
 
-user = files[0]
+for file in files[3:]:
+    username = file.split('.csv')[0]
+    print(username)
+    years = []
 
-years = []
+    df = pd.read_csv(path + file)
 
-df = pd.read_csv(path + user)
+    for i in df.date:
+        y = i.split('-')[0]
+        years.append(y)
 
-for i in df.date:
-    y = i.split('-')[0]
-    years.append(y)
+    years = list(set(years))
 
-years = list(set(years))
+    for i in years:
+        p = 'Data/Grouped_Datas/' + username + '/' + i + '/'
+        if not os.path.exists(p):
+            os.makedirs(p)
 
-for i in years:
-    p = 'Data/Grouped_Datas/' + user.split('.csv')[0] + '/' + i + '/'
-    if not os.path.exists(p):
-        os.makedirs(p)
+    for year in years:
+        one = []
+        two = []
+        three = []
 
-one = []
-two = []
-three = []
+        for index, row in df.iterrows():
+            m = int(row['date'].split('-')[1])
+            y = row['date'].split('-')[0]
+            if m <= 4 and year == y:
+                one.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
+            elif m <= 8 and year == y:
+                two.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
+            elif m <= 12 and year == y:
+                three.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
 
-for index, row in df.iterrows():
-    y = int(row['date'].split('-')[1])
-    if y <= 4:
-        one.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
-    elif y <= 8:
-        two.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
-    else:
-        three.append([row['tweet_id'], row['date'], row['title'], row['category'], row['rating'], row['urls']])
+        write_file(username, year, one, 'first')
+        write_file(username, year, two, 'second')
+        write_file(username, year, three, 'third')
 
-print(one)
-
-dataframe = pd.DataFrame(one, columns=['tweet_id', 'date', 'title', 'category', 'rating', 'urls'])
-dataframe['date'] = pd.to_datetime(dataframe['date'])
-dataframe.sort_values(by=['date'], inplace=True, ascending=False)
-dataframe.to_csv('Data/Grouped_Datas/50centwingnight/2019' + 'one.csv', index=None, header=True)
-
-'''for index, row in df.iterrows():
-    y = row['date'].split('-')[0]
-    years[y].append(row)
-
-path = 'Tweet_Objects/' + name + '/'
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-print(years)'''
+print('Done')
